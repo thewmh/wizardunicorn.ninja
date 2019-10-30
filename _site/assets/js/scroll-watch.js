57ed0cc0ede5441c3f48b3ded38b7409886f6bf0
William Harrison <wharrison@horizontalintegration.com>
2019-10-30 14:18:53 -0500

    added worknotes for days and updated the scroll watch js file

--- _site/assets/js/scroll-watch.js
+++ _site/assets/js/scroll-watch.js
@@ -1,8 +1,29 @@
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
+// function myFunction() {
+//   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//   var scrolled = (winScroll / height) * 100;
+//   document.getElementById("myBar").style.width = scrolled + "%";
+// }
+
+let prev_position = window.pageYOffset || document.body.scrollTop;
+
+window.addEventListener('scroll', function(e) {
+    var s = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    var content = document.getElementById('main-content');
+    var d = Math.max(content.clientHeight, content.scrollHeight, content.offsetHeight);
+    var position = (s / d) * 100;
+    document.getElementById('progress').setAttribute('style', 'width: ' + position + '%');
+    // if(s <= 493 || (s >= 494 && s <= 693 && s > prev_position)) {
+    //     console.log('below 693px');
+    //     document.getElementById('main-content').setAttribute('style', 'height: 700px; width: 100%; position: fixed; transform: translateY(-' + s +'px); transition: transform 0s;');
+    // } else if(s >= 694 && s > prev_position) {
+    //     console.log('increased above 694px');
+    //     document.getElementById('main-content').setAttribute('style', 'height: 700px; width: 100%; position: fixed; transform: translateY(-694px); transition: transform .5s ease, height .5s ease;');
+    // } else {
+    //     document.getElementById('main-content').setAttribute('style', 'height: 200px; width: 100%; position: fixed; transform: translateY(0); transition: transform .5s ease;');
+    //     console.log('decreasing but above 694px');
+    // };
+    prev_position = s <= 0 ? 0 : s;
+});
