cf274feb8f83ea0ff6261dd3535a9c0c250cf598
William Harrison <wharrison@horizontalintegration.com>
2020-01-15 07:25:31 -0600

    cleaning up log errors for la-croix.js and scroll-watch progress bar

--- assets/js/scroll-watch.js
+++ assets/js/scroll-watch.js
@@ -1,10 +1,9 @@
 let prev_position = window.pageYOffset || document.body.scrollTop;
 
 window.addEventListener('scroll', function(e) {
-    var s = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    let s = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
     let d = (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-    console.log(s, d)
-    var position = (s / d) * 100;
+    let position = (s / d) * 100;
     document.getElementById('progress').setAttribute('style', 'width: ' + position + '%');
     prev_position = s <= 0 ? 0 : s;
 });
