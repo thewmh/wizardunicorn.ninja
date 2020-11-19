7587f67b48d1b20991f299c24414e03763d8f095
William Harrison <wharrison@horizontal.com>
2020-11-19 08:49:24 -0600

    updates workflow related files

--- test/build.sh
+++ test/build.sh
@@ -3,6 +3,6 @@ echo '👍 ENTRYPOINT HAS STARTED—INSTALLING THE GEM BUNDLE'
 bundle install
 bundle list | grep "jekyll ("
 echo '👍 BUNDLE INSTALLED—BUILDING THE SITE'
-bundle exec jekyll build
+npm run build
 echo '👍 THE SITE IS BUILT'
 npm run postbuild
