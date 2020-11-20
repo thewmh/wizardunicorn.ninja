e86d28d3b7d249a1c407410d9c02dbdf36450c15
William Harrison <wharrison@horizontal.com>
2020-11-20 09:13:04 -0600

    updates .gitignore, config, and adds new rules to purgecss

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,10 +1,13 @@
 module.exports = {
     // These are the files that Purgecss will search through
     content: ["./_site/**/*.html"],
+    blocklist: ["./_site/js30"],
 
     // These are the stylesheets that will be subjected to the purge
     css: ["./_site/assets/css/*.css"],
-    whitelist: ['la-croix-can'],
+    safelist: {
+        standard: ['la-croix-can']
+    },
     
     // We make it the files here
     output: ["./_site/assets/css"]
