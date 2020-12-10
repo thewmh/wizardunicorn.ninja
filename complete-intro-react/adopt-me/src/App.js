06d5a7d72c2cf166648e303dbb3c74bc2b0b33b4
William Harrison <wharrison@horizontal.com>
2020-12-10 12:11:06 -0600

    trying to fix paths for react app

--- complete-intro-react/adopt-me/src/App.js
+++ complete-intro-react/adopt-me/src/App.js
@@ -4,15 +4,17 @@ import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 import ThemeContext from "./ThemeContext";
+import { BaseRouter, useBasepath } from "./Basepath";
 
 const App = () => {
+  const basepath = useBasepath();
   const themeHook = useState("orange");
   return (
     <React.StrictMode>
       <ThemeContext.Provider value={themeHook}>
         <div>
           <header>
-            <Link to="./">Adopt Me!</Link>
+            <Link to={`${basepath}`}>Adopt Me!</Link>
           </header>
           <Router>
             <SearchParams path="/" />
