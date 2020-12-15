c0726c1afdf2f185179652666f5edb7e110aa423
William Harrison <wharrison@horizontal.com>
2020-12-15 15:19:40 -0600

    updates build file

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-echo '👍 ENTRYPOINT HAS STARTED—INSTALLING THE GEM BUNDLE'
-bundle install
-bundle list | grep "jekyll ("
-echo '👍 BUNDLE INSTALLED—BUILDING THE SITE'
 npm run build
 echo '👍 THE SITE IS BUILT'
