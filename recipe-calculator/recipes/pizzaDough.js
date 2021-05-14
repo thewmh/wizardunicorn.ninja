db9966d50410dd7cc8927bbb6f56d742332ceea6
thewmh <thewmh@users.noreply.github.com>
2021-05-14 20:57:30 +0000

    deploy: 1aebe3f0701438d4ecff5553955f76d65f3a89ba

---
+++ recipe-calculator/recipes/pizzaDough.js
@@ -0,0 +1,36 @@
+{
+  "recipeData": [{
+    "name": "Pizza Dough",
+    "ingredients": [
+        {
+            "id": "whiteFlour",
+            "name": "White Flour",
+            "amount": 500
+        },
+        {
+            "id" :"water",
+            "name": "Water",
+            "amount": 500
+        },
+        {
+            "id": "instantYeast",
+            "name": "Instant Yeast",
+            "amount": 0.4
+        },
+        {
+            "id": "whiteFlour2",
+            "name": "White Flour",
+            "amount": 500
+        },
+        {
+            "id": "water2",
+            "name": "Water",
+            "amount": 250
+        },
+        {
+            "id": "seaSalt",
+            "name": "Fine Sea Salt",
+            "amount": 20
+        }
+    ]
+}]}
