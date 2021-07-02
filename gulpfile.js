dbdf0c1b076bb518e637d10c23ebb26510f8dea8
William Harrison <wharrison@horizontal.com>
2021-07-02 07:19:30 -0500

    adds gulp to minify and concatenate js

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+const {dest, src, watch, series, parallel} = require('gulp');
+const concat = require('gulp-concat');
+const plumber = require('gulp-plumber');
+const terser = require('gulp-terser');
+
+// Transpile, concatenate and minify scripts
+function js() {
+  return src('./assets/js/**/*', {sourcemaps: false})
+      .pipe(plumber())
+      .pipe(terser())
+      .pipe(concat('wizardUnicornNinja.js'))
+      .pipe(dest('_site/assets/js', {sourcemaps: '.'}))
+}
+
+exports.js = js;
